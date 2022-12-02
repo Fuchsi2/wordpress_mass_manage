1777a64ac72125bc389ec04815ec9a8681aeb42b
Seb.Fxs <Fuchs.basti@gmail.com>
2022-12-02 09:41:19 +0100

    Update run.sh

--- installer/run.sh
+++ installer/run.sh
@@ -2,11 +2,12 @@
 
 echo "{\"cwd\":\"./\",\"domain\":\"$wmm_domain\"}" > /install/wmm-conf.json
 
+echo "setting up pkg"
 wget -q https://github.com/vercel/pkg-fetch/releases/download/v3.4/node-v16.16.0-linux-x64
 wget -q https://github.com/vercel/pkg-fetch/releases/download/v3.4/node-v16.16.0-win-x64
 wget -q https://github.com/vercel/pkg-fetch/releases/download/v3.4/node-v16.16.0-macos-x64
 mv node-v16.16.0-* PKG_CACHE_PATH
-
+echo "running build"
 rm /install/build/*
 npm run build
 cp /install/build/* /workspace/
